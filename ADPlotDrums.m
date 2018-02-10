fe7a50460bad370817b3161f46c4b9ea2d69761d
Gal Lifshitz <gallif@post.bgu.ac.il>
2018-02-10 17:16:14 +0200

    plot

---
+++ ADPlotDrums.m
@@ -0,0 +1,7 @@
+function ADPlotDrums(fig,params)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ ADPlotStickLocs.m
@@ -0,0 +1,8 @@
+function ADPlotStickLocs(f,stickLoc,params)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end
+
