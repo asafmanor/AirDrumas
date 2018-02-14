fd9adfc452c76603be056d3f472c1083d2a096ad
Gal Lifshitz <gallif@post.bgu.ac.il>
2018-02-14 10:09:11 +0200

    added offline mode

--- galTestRun.m
+++ galTestRun.m
@@ -21,6 +21,9 @@ clear all; close all;
 % y = (sin(2*t).^2).*cos(t);
 % comet3(x,y,t,0.01);
 load('C:\Users\glifshitz\Documents\GitHub\AirDrums\Offline\offlineData.mat')
-load('C:\Users\glifshitz\Documents\GitHub\AirDrums\Offline\record1.mat')
-load('C:\Users\glifshitz\Documents\GitHub\AirDrums\Offline\record2.mat')
+clear offlineData;
+%load('C:\Users\glifshitz\Documents\GitHub\AirDrums\Offline\record1.mat')
+ load('C:\Users\glifshitz\Documents\GitHub\AirDrums\Offline\record2.mat')
+
+ADOfflineModeDisplay(offlineData,params);
 
